3ef6c50d72435c1fc5dbcfe090a518e8ce4b421e
Muhammad Fadillah <dhiell.ghodill@gmail.com>
2018-04-26 15:15:15 +0700

    koneksi db

---
+++ src/reservasihotel/koneksi.java
@@ -0,0 +1,49 @@
+package reservasihotel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dosen
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+public class koneksi {
+    
+     Connection con;
+    Statement stm;
+    private static Connection mysqlkonek;
+    public static Connection koneksiDB() throws SQLException {
+        if(mysqlkonek==null){
+            try {
+                String DB="jdbc:mysql://localhost:3306/amazing"; // delta_db database
+                String user="root"; // user database
+                String pass=""; // password database
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                
+                mysqlkonek = (Connection) DriverManager.getConnection(DB,user,pass);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,"gagal koneksi");
+            }
+        }
+        return mysqlkonek;
+    }
+    
+     public void config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/amazing", "root", "");
+            stm = con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "koneksi gagal "+e.getMessage());
+        }
+    }
+}
